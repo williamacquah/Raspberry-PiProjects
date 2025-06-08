711ef5b728f46cb0459b9b9c881d02d7446f455c
williamacquah <77838879+williamacquah@users.noreply.github.com>
2025-06-08 17:13:39 +0200

    Delete led_control directory

--- led_control/README.md
+++
@@ -1,5 +0,0 @@
-# LED Control with GPIO on Raspberry Pi
-
-This project demonstrates how to control an LED connected to GPIO18 using both terminal commands and a C program.
-
-
--- led_control/terminal_commands.sh
+++
@@ -1,4 +0,0 @@
-echo 18 | sudo tee /sys/class/gpio/export
-echo out | sudo tee /sys/class/gpio/gpio18/direction
-echo 1 | sudo tee sys/class/gpio/gpio18/value
-echo 0 | sudo tee sys/class/gpio/gpio18/value
