e32572287fc99258d030eae371c983def0af9350
William Acquah <willit1479@gmail.com>
2025-06-08 16:39:27 +0200

    Added SocketsII folder with project files

---
+++ build/CMakeFiles/Hello-DES.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Hello-DES.dir/main.c.o"
+  "Hello-DES"
+  "Hello-DES.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Hello-DES.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
